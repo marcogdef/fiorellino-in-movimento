4fa6e28835244115ba178e9188e13c5e5f5a514d
marcogdef <defronzomarco@gmail.com>
2022-11-05 08:45:54 +0100

    fiorellino in movimento finito

--- static/js/java.js
+++ static/js/java.js
@@ -3,16 +3,28 @@ let y
 let d=50
 let xf=d
 let yf=d
-let xspeed=5
-let yspeed=2
+let xmov=5
+let ymov=2
 
 function setup(){
-    createCanvas(400,400)
+    createCanvas(1000,1000)
     strokeWeight(2)
-    frameRate(2)
+    frameRate(6)
     x = random(50, 900)
     y = random(50, 900)
 }
+
+function draw(){
+    background(0, 255, 51)
+    fiorellino(x, y, d)
+    x += xmov;
+    y += ymov;
+    if(x>1000-d || x<d){
+        xmov = -xmov
+    }
+    if(y>1000-d || y<d){
+        ymov = -ymov
+    }
 
 function fiorellino(x, y, d){
     fill(255, 190, 0)
@@ -22,17 +34,5 @@ function fiorellino(x, y, d){
     circle(x+d/2, y+d/2, d)
     fill(255, 120, 0)
     circle(x, y, d)
-}
-
-function draw(){
-    background(0, 255, 51)
-    fiorellino(x, y, d)
-    x += xspeed
-    y += yspeed
-    if(x>400-d || x<d){
-        xspeed = -xspeed
-    }
-    if(y>400-d || y<d){
-        yspeed = -yspeed
     }
 }
